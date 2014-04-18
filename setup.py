24c9c92b14a1e2b7bef294e18620e28d08bd37e1
Sergey Tikhonov <zimbler@gmail.com>
2014-04-18 12:58:16 +0400

    0.6.4 bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='async_redis',
-    version='0.6.3',
+    version='0.6.4',
     packages=['async_redis'],
     url='',
     license='Beer Licence',
