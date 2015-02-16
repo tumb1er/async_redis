02ae871e81aa7793e10be3326c65cb1d82f384b4
Sergey Tikhonov <zimbler@gmail.com>
2015-02-16 12:54:52 +0300

    fix #3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='async_redis',
-    version='0.8.1',
+    version='0.9.0',
     packages=['async_redis'],
     url='',
     license='Beer Licence',
